5aa1e66c048a2a08a7e8e6678d25de99687f322f
sazzadshakhbd <sazzadshakhbd@gmail.com>
2025-04-25 21:30:33 +0600

    add Hero Text and Layout Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Hero from "./sections/Hero"
+
 const App = () => {
   return (
     <main>
-      <div>
-      <h1 className="text-3xl underline">Welcome to my 3d portfolio</h1>
-    </div>
+      <Hero />
     </main>
   )
 }
